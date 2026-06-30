4070246c029609633382166509cef31a11f6e71e
David Nye <davidgn@gmail.com>
2026-06-30 06:35:26 -0400

    Add MercadoLibre spider for Chile

---
+++ src/chile_mercadolibre_spider.py
@@ -0,0 +1,18 @@
+import re
+from html_search_spider import HTMLSearchSpider
+
+class ChileMercadolibreSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages: int = 50):
+        super().__init__(
+            platform_name="MercadoLibre_CL", base_url="https://listado.mercadolibre.cl",
+            search_path="/{search_term}#D[A:{search_term}]",
+            selectors={'container': '.ui-search-result__wrapper', 'title': 'h2', 'price': '.andes-money-amount__fraction'},
+            territory="Chile", limit_pages=limit_pages
+        )
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", type=str, default="libros")
+    args = parser.parse_args()
+    ChileMercadolibreSpider(limit_pages=1).run(search_term=args.query)
