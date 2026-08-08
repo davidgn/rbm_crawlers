0f31ee2a2d79f80b1773a27247019383f124f45a
David Nye <davidgn@gmail.com>
2026-08-08 13:38:22 -0400

    Pass221 M-Bag Expansion Batch 47: Add 3 spiders + 3 registry entries + Milestone 100

---
+++ src/yordam_tr_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class YordamTrSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Yordam Kitap",
+            base_url="https://www.yordamkitap.com",
+            search_path="arama?q={query}",
+            territory="Turkey",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="TRY",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = YordamTrSpider(limit_pages=1, limit_items=5)
+    spider.run()
