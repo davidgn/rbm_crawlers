bc49eac823281c8407ae0220fdbd20b8fb601f31
David Nye <davidgn@gmail.com>
2026-08-27 16:10:19 -0400

    Batch 114 M-Bag country expansion: Ateneo Press, UP Press, UST Publishing House

---
+++ src/uppress_ph_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class UpPressSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="UP Press",
+            base_url="https://upress.up.edu.ph",
+            search_path="search?q={query}",
+            territory="Philippines",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="PHP",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = UpPressSpider(limit_pages=1, limit_items=5)
+    spider.run()
