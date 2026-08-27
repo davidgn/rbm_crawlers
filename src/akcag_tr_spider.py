a808b46ad466b81961be362bdc2683d64bb8021b
David Nye <davidgn@gmail.com>
2026-08-27 13:05:56 -0400

    Batch 106 M-Bag country expansion: Sozcu Kitapevi, TDV Kitap, Akcag

---
+++ src/akcag_tr_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class AkcagSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Akcag",
+            base_url="https://akcag.com.tr",
+            search_path="search?q={query}",
+            territory="Turkey",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="TRY",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = AkcagSpider(limit_pages=1, limit_items=5)
+    spider.run()
