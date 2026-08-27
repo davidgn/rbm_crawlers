f8ce84c2f6e04de4a16977494bce32dddf78d298
David Nye <davidgn@gmail.com>
2026-08-27 13:06:19 -0400

    Batch 108 M-Bag country expansion: Libreria Francesa, Tango Discos, Panamericana V2

---
+++ src/panamericana_co_v2_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class PanamericanaV2Spider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Panamericana V2",
+            base_url="https://panamericana.com.co",
+            search_path="search?q={query}",
+            territory="Colombia",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="COP",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = PanamericanaV2Spider(limit_pages=1, limit_items=5)
+    spider.run()
