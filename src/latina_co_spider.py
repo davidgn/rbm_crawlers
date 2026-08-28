67986ca4f4dbf8da23f4589fe3eec06ead003424
David Nye <davidgn@gmail.com>
2026-08-27 22:56:09 -0400

    Batch 140 M-Bag country expansion: Libreria Latina, Libreria Catolica, Libreria Bogota

---
+++ src/latina_co_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class LibreriaLatinaSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Libreria Latina",
+            base_url="https://librerialatina.com.co",
+            search_path="search?q={query}",
+            territory="Colombia",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="COP",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = LibreriaLatinaSpider(limit_pages=1, limit_items=5)
+    spider.run()
