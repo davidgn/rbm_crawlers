28d5a8b98a255abc989bbcc719eeb5a78af8c5ce
David Nye <davidgn@gmail.com>
2026-08-27 20:20:56 -0400

    Batch 131 M-Bag country expansion: Kotobgy, Eqra Books, Kutub Egypt

---
+++ src/eqra_eg_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class EqraBooksSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Eqra Books",
+            base_url="https://eqra.com.eg",
+            search_path="search?q={query}",
+            territory="Egypt",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="EGP",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = EqraBooksSpider(limit_pages=1, limit_items=5)
+    spider.run()
---
+++ src/kutub_eg_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class KutubEgyptSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Kutub Egypt",
+            base_url="https://kutub.com.eg",
+            search_path="search?q={query}",
+            territory="Egypt",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="EGP",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = KutubEgyptSpider(limit_pages=1, limit_items=5)
+    spider.run()
