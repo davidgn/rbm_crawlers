215b03b1cd8bcba3d28a7d91f39696427fab16a9
David Nye <davidgn@gmail.com>
2026-08-27 22:55:14 -0400

    Batch 138 M-Bag country expansion: Sun Africa Books, Sutherland Books, Vintage Books ZA

---
+++ src/sunafrica_za_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class SunAfricaBooksSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Sun Africa Books",
+            base_url="https://sunafricabooks.co.za",
+            search_path="search?q={query}",
+            territory="South Africa",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="ZAR",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = SunAfricaBooksSpider(limit_pages=1, limit_items=5)
+    spider.run()
