22cda0ff62cd827fee3c7de81d029c1a9824df29
David Nye <davidgn@gmail.com>
2026-08-27 20:18:56 -0400

    Batch 123 M-Bag country expansion: Saigon Books, Quang Van Books, MegaTexts

---
+++ src/quangvan_vn_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class QuangVanBooksSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Quang Van Books",
+            base_url="https://quangvanbooks.com",
+            search_path="search?q={query}",
+            territory="Vietnam",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="VND",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = QuangVanBooksSpider(limit_pages=1, limit_items=5)
+    spider.run()
