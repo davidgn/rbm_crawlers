36376874a68dd56f56fe7831068ab9510494702b
David Nye <davidgn@gmail.com>
2026-08-27 22:56:38 -0400

    Batch 141 M-Bag country expansion: Siglo XXI Editores CO, Bookland PK, Bismillah Books

---
+++ src/sigloxxi_co_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class SigloXxiEditoresCoSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Siglo XXI Editores CO",
+            base_url="https://sigloxxieditores.com.co",
+            search_path="search?q={query}",
+            territory="Colombia",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="COP",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = SigloXxiEditoresCoSpider(limit_pages=1, limit_items=5)
+    spider.run()
