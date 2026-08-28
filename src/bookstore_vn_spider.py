54870e538a3d10cd2f3c0f60337e69cd8743679f
David Nye <davidgn@gmail.com>
2026-08-27 20:18:33 -0400

    Batch 122 M-Bag country expansion: Bookstore VN, Sach Noi Viet, Nha Sach Giao Duc

---
+++ src/bookstore_vn_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class BookstoreVnSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Bookstore VN",
+            base_url="https://bookstore.vn",
+            search_path="search?q={query}",
+            territory="Vietnam",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="VND",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = BookstoreVnSpider(limit_pages=1, limit_items=5)
+    spider.run()
