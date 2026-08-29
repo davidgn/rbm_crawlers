374e50a83b968646661b3f590a5038c5c27140a9
David Nye <davidgn@gmail.com>
2026-08-29 14:51:41 -0400

    Batch 477 M-Bag country expansion (Verified Domains): Livraria Mandarina, Tapera Tapera, Libreria Marco Polo

---
+++ src/mandarina_br_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class LivrariaMandarinaSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Livraria Mandarina",
+            base_url="https://livrariamandarina.com.br",
+            search_path="search?q={query}",
+            territory="Brazil",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="BRL",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = LivrariaMandarinaSpider(limit_pages=1, limit_items=5)
+    spider.run()
