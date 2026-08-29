c7223cbbeddd962d4a795cc5a4323ced1abda060
David Nye <davidgn@gmail.com>
2026-08-28 21:19:35 -0400

    Batch 148 M-Bag country expansion (Verified Domains): Libreria de la Biblioteca Nacional, Woopy, Libros del Pasaje

---
+++ src/librosdelpasaje_ar_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class LibrosDelPasajeSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Libros del Pasaje",
+            base_url="https://www.librosdelpasaje.com.ar",
+            search_path="search?q={query}",
+            territory="Argentina",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="ARS",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = LibrosDelPasajeSpider(limit_pages=1, limit_items=5)
+    spider.run()
