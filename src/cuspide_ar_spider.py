7d3e4945d53cad8b51c027f4ea3c2aa5ea6d670b
David Nye <davidgn@gmail.com>
2026-08-29 00:21:03 -0400

    Batch 281 M-Bag country expansion (Verified Domains): Mr Books EC, Cuspide Libros, El Ateneo AR

---
+++ src/cuspide_ar_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class CuspideLibrosSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Cuspide Libros",
+            base_url="https://www.cuspide.com",
+            search_path="search?q={query}",
+            territory="Argentina",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="ARS",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = CuspideLibrosSpider(limit_pages=1, limit_items=5)
+    spider.run()
