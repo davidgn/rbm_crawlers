5fe4461a4bbfa7c0db133eeab0dce01380f8a89b
David Nye <davidgn@gmail.com>
2026-08-28 22:14:33 -0400

    Batch 184 M-Bag country expansion (Verified Domains): Readings AU, The Nile AU, The Nile NZ

---
+++ src/thenile_nz_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class TheNileNzSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="The Nile NZ",
+            base_url="https://www.thenile.co.nz",
+            search_path="search?q={query}",
+            territory="New Zealand",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="NZD",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = TheNileNzSpider(limit_pages=1, limit_items=5)
+    spider.run()
