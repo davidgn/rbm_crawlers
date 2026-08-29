c23400f2d8343f47f32ef6050e16f2f371a061ff
David Nye <davidgn@gmail.com>
2026-08-28 21:19:59 -0400

    Batch 149 M-Bag country expansion (Verified Domains): Librerias Crisol, Ibero Librerias, Librerias SBS

--- src/ibero_pe_spider.py
+++ src/ibero_pe_spider.py
@@ -1,10 +1,10 @@
 from html_search_spider import HTMLSearchSpider
 
-class LibreriasCrisolSpider(HTMLSearchSpider):
+class IberoLibreriasSpider(HTMLSearchSpider):
     def __init__(self, limit_pages=5, limit_items=None, **kwargs):
         super().__init__(
-            platform_name="Librerias Crisol",
-            base_url="https://www.crisol.com.pe",
+            platform_name="Ibero Librerias",
+            base_url="https://iberolibrerias.com",
             search_path="search?q={query}",
             territory="Peru",
             limit_pages=limit_pages,
@@ -16,5 +16,5 @@ class LibreriasCrisolSpider(HTMLSearchSpider):
 if __name__ == "__main__":
     import logging
     logging.basicConfig(level=logging.INFO)
-    spider = LibreriasCrisolSpider(limit_pages=1, limit_items=5)
+    spider = IberoLibreriasSpider(limit_pages=1, limit_items=5)
     spider.run()
--- src/sbs_pe_spider.py
+++ src/sbs_pe_spider.py
@@ -1,31 +1,20 @@
 from html_search_spider import HTMLSearchSpider
 
-class SbsPeSpider(HTMLSearchSpider):
-    """
-    Broad crawler for SBS Librería Internacional (Peru) using HTML search parsing.
-    Prices are in Peruvian Soles (PEN).
-    """
-    def __init__(self, limit_pages: int = 5):
+class LibreriasSbsSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
         super().__init__(
-            platform_name="SBS Libreria Internacional",
+            platform_name="Librerias SBS",
             base_url="https://www.sbs.com.pe",
-            search_path="catalogsearch/result/?q={query}",
-            selectors={
-                "container": "li.product-item",
-                "title": "a.product-item-link",
-                "author": "div.author-name",
-                "price": "span.price",
-                "url": "a.product-item-link"
-            },
+            search_path="search?q={query}",
             territory="Peru",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
             price_currency="PEN",
-            limit_pages=limit_pages
+            **kwargs
         )
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--limit-pages", type=int, default=5)
-    args = parser.parse_args()
-
-    SbsPeSpider(limit_pages=args.limit_pages).run()
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = LibreriasSbsSpider(limit_pages=1, limit_items=5)
+    spider.run()
