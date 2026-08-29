15b61057230b928485b4e9bd810aa2824577e28b
David Nye <davidgn@gmail.com>
2026-08-29 04:18:57 -0400

    Batch 394 M-Bag country expansion (Verified Domains): Libreria Berkana, Calamo, Corregidor

---
+++ src/calamo_es_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class CalamoSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Calamo",
+            base_url="https://www.calamo.com",
+            search_path="search?q={query}",
+            territory="Spain",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="EUR",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = CalamoSpider(limit_pages=1, limit_items=5)
+    spider.run()
