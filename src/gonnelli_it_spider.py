2c5ef78fe46055f2b9a8a99065f78855eb9cc243
David Nye <davidgn@gmail.com>
2026-08-29 04:14:42 -0400

    Batch 373 M-Bag country expansion (Verified Domains): Moser Buch, Libreria Hoepli, Libreria Gonnelli

---
+++ src/gonnelli_it_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class LibreriaGonnelliSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Libreria Gonnelli",
+            base_url="https://www.gonnelli.it",
+            search_path="search?q={query}",
+            territory="Italy",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="EUR",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = LibreriaGonnelliSpider(limit_pages=1, limit_items=5)
+    spider.run()
