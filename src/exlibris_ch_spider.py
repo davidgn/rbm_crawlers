13a8d741708f13ba77e97af9bf88fe151fbee883
David Nye <davidgn@gmail.com>
2026-08-29 03:42:41 -0400

    Batch 289 M-Bag country expansion (Verified Domains): Ex Libris CH, Payot CH, BKM Kitap

---
+++ src/exlibris_ch_spider.py
@@ -0,0 +1,20 @@
+from html_search_spider import HTMLSearchSpider
+
+class ExLibrisChSpider(HTMLSearchSpider):
+    def __init__(self, limit_pages=5, limit_items=None, **kwargs):
+        super().__init__(
+            platform_name="Ex Libris CH",
+            base_url="https://www.exlibris.ch",
+            search_path="search?q={query}",
+            territory="Switzerland",
+            limit_pages=limit_pages,
+            limit_items=limit_items,
+            price_currency="CHF",
+            **kwargs
+        )
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    spider = ExLibrisChSpider(limit_pages=1, limit_items=5)
+    spider.run()
